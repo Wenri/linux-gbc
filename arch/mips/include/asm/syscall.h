41579eabbec9d3425c78b9e8c1f6a6f8a24b84d3
Markos Chandras <markos.chandras@imgtec.com>
2014-04-22 15:40:36 +0100

    MIPS: Add new AUDIT_ARCH token for the N32 ABI on MIPS64
    A MIPS64 kernel may support ELF files for all 3 MIPS ABIs
    (O32, N32, N64). Furthermore, the AUDIT_ARCH_MIPS{,EL}64 token
    does not provide enough information about the ABI for the 64-bit
    process. As a result of which, userland needs to use complex
    seccomp filters to decide whether a syscall belongs to the o32 or n32
    or n64 ABI. Therefore, a new arch token for MIPS64/n32 is added so it
    can be used by seccomp to explicitely set syscall filters for this ABI.
    
    Change-Id: Ie03bd32672f40ce309170d497f3e1875a02b9cb0
    Signed-off-by: Markos Chandras <markos.chandras@imgtec.com>
    Acked-by: Eric Paris <eparis@redhat.com>
    Acked-by: Paul Moore <pmoore@redhat.com>
    Cc: Andy Lutomirski <luto@amacapital.net>
    Cc: Ralf Baechle <ralf@linux-mips.org>
    Cc: Andy Lutomirski <luto@amacapital.net>
    Cc: linux-mips@linux-mips.org
    Link: http://sourceforge.net/p/libseccomp/mailman/message/32239040/
    Patchwork: https://patchwork.linux-mips.org/patch/6818/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    Signed-off-by: Shuangshuang Zhang <zhangshuangshuang@loongson.cn>

--- arch/mips/include/asm/syscall.h
+++ arch/mips/include/asm/syscall.h
@@ -134,6 +134,8 @@ static inline int syscall_get_arch(struct task_struct *task,
 #ifdef CONFIG_64BIT
 	if (!test_tsk_thread_flag(task, TIF_32BIT_REGS))
 		arch |= __AUDIT_ARCH_64BIT;
+	if (test_thread_flag(TIF_32BIT_ADDR))
+		arch |= __AUDIT_ARCH_CONVENTION_MIPS64_N32;
 #endif
 #if defined(__LITTLE_ENDIAN)
 	arch |=  __AUDIT_ARCH_LE;
