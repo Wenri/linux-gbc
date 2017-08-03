f31fad1d63ab1638cf50e82760a3e68da9c03561
yangxiaojuan <yangxiaojuan@loongson.cn>
2017-08-03 10:56:12 +0800

    MIPS: Loongson3: Replace phys_t with phys_addr_t
    Change-Id: Iff58c0cf0fc4b03b5f35a1effaf2fecd3ada5d55
    Signed-off-by: Shuangshuang Zhang <zhangshuangshuang@loongson.cn>

--- arch/mips/include/asm/mach-loongson/ioremap.h
+++ arch/mips/include/asm/mach-loongson/ioremap.h
@@ -15,13 +15,13 @@
  * Allow physical addresses to be fixed up to help peripherals located
  * outside the low 32-bit range -- generic pass-through version.
  */
-static inline phys_t fixup_bigphys_addr(phys_t phys_addr, phys_t size)
+static inline phys_addr_t fixup_bigphys_addr(phys_addr_t phys_addr, phys_addr_t size)
 {
 	return phys_addr;
 }
 
 /* Add support for uncached accelerate */
-static inline void __iomem *plat_ioremap(phys_t offset, unsigned long size,
+static inline void __iomem *plat_ioremap(phys_addr_t offset, unsigned long size,
 	unsigned long flags)
 {
 #ifdef CONFIG_64BIT
