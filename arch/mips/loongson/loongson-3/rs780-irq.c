d2da8ed238fff7bac5f84bd4566f4f820360c609
Xue Liu <liuxue@loongson.cn>
2019-04-04 18:12:55 +0800

    MIPS: LSVZ: Fix "genirq: Flags mismatch irq 8. (rtc0) vs. (virtio1)"
    All 8259A interrupts work fine for virtual machine, prefer not to share irq.
    
    Change-Id: I0b0f7937443bcbc6f83347e4d94a0d2ae83d96dd

--- arch/mips/loongson/loongson-3/rs780-irq.c
+++ arch/mips/loongson/loongson-3/rs780-irq.c
@@ -13,7 +13,11 @@ extern void loongson3_send_irq_by_ipi(int cpu, int irqs);
 extern void loongson3_ipi_interrupt(struct pt_regs *regs);
 
 unsigned int irq_cpu[16] = {[0 ... 15] = -1};
+#ifdef CONFIG_KVM_GUEST_LS3A3000
+unsigned int ht_irq[] = {0, 1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+#else
 unsigned int ht_irq[] = {0, 1, 3, 4, 5, 6, 7, 8, 12, 14, 15};
+#endif
 unsigned int local_irq = 1<<0 | 1<<1 | 1<<2 | 1<<7 | 1<<8 | 1<<12;
 
 static int bootcore_int_mask2 = 1;
