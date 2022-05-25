71a05b78c1444fee8605a153ede6f7591cdfa752
13r0ck <bnr@tuta.io>
2022-05-24 19:02:56 -0600

    Adds an ALSA mapping for Gigabyte Pro WiFi Rev 2.1
    This patch was written by Takashi Iwai, I am just
    commiting it into the pop-os kernel before this
    is upstreamed

--- sound/usb/mixer_maps.c
+++ sound/usb/mixer_maps.c
@@ -624,7 +624,6 @@ static const struct usbmix_ctl_map usbmix_ctl_maps[] = {
 	{	/* Gigabyte B450/550 Mobo */
 		.id = USB_ID(0x0414, 0xa00d),
 		.map = gigabyte_b450_map,
-		.connector_map = gigabyte_b450_connector_map,
 	},
 	{	/* ASUS ROG Zenith II (main audio) */
 		.id = USB_ID(0x0b05, 0x1916),
