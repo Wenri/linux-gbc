cb120943b0ae2fe30c98b5a631cf170b73ec3525
Jinke Han <hanjinke.666@bytedance.com>
2023-05-08 01:06:31 +0800

    UBUNTU: SAUCE: blk-throttle: Fix io statistics for cgroup v1
    After commit f382fb0bcef4 ("block: remove legacy IO schedulers"),
    blkio.throttle.io_serviced and blkio.throttle.io_service_bytes become
    the only stable io stats interface of cgroup v1, and these statistics
    are done in the blk-throttle code. But the current code only counts the
    bios that are actually throttled. When the user does not add the throttle
    limit, the io stats for cgroup v1 has nothing. I fix it according to the
    statistical method of v2, and made it count all ios accurately.
    
    Fixes: a7b36ee6ba29 ("block: move blk-throtl fast path inline")
    Tested-by: Andrea Righi <andrea.righi@canonical.com>
    Signed-off-by: Jinke Han <hanjinke.666@bytedance.com>
    (cherry picked from https://lore.kernel.org/lkml/20230507170631.89607-1-hanjinke.666@bytedance.com/t/)
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- block/blk-cgroup.c
+++ block/blk-cgroup.c
@@ -2071,6 +2071,9 @@ void blk_cgroup_bio_start(struct bio *bio)
 	struct blkg_iostat_set *bis;
 	unsigned long flags;
 
+	if (!cgroup_subsys_on_dfl(io_cgrp_subsys))
+		return;
+
 	/* Root-level stats are sourced from system-wide IO stats */
 	if (!cgroup_parent(blkcg->css.cgroup))
 		return;
@@ -2101,8 +2104,7 @@ void blk_cgroup_bio_start(struct bio *bio)
 	}
 
 	u64_stats_update_end_irqrestore(&bis->sync, flags);
-	if (cgroup_subsys_on_dfl(io_cgrp_subsys))
-		cgroup_rstat_updated(blkcg->css.cgroup, cpu);
+	cgroup_rstat_updated(blkcg->css.cgroup, cpu);
 	put_cpu();
 }
 
